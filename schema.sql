b5b6860312a481b0f134ed29b5ae9a01b0313a4f
Mike <joeeoj@blueskyemail.net>
2023-02-05 23:55:37 -0800

    update cols and add never_seen_sofa view

--- schema.sql
+++ schema.sql
@@ -1,8 +1,22 @@
-CREATE TABLE IF NOT EXISTS sofas (
-    id INTEGER PRIMARY KEY AUTOINCREMENT
-    unique_id VARCHAR(32) UNIQUE,
-    title VARCHAR,
+CREATE TABLE IF NOT EXISTS items (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    item_id VARCHAR(32) UNIQUE,
+    product_type VARCHAR(32),
+    title VARCHAR(100),
     sold BOOLEAN,
     price FLOAT,
-    url VARCHAR
+    url VARCHAR,
+    alert_sent_dt DATETIME
 );
+
+CREATE VIEW IF NOT EXISTS never_seen_sofas
+AS
+SELECT *
+FROM items
+WHERE
+    product_type = 'sofa'
+    AND sold = False
+    AND alert_sent_dt IS NULL
+ORDER BY
+    price
+;
